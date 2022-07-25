3ed5c2899ef76a6c0d143ad21478fb1536aa0547
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-25 11:51:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import 'modern-normalize/modern-normalize.css';
+import App from 'components/App';
+// import './index.css';
+
+import { theme } from 'constants';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from 'components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
+
